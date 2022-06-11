846f35f1b389a1077bf9b59944e52d0d5689d0be
Paulina Bury <burabury@MacBook-Pro-Paulina.local>
2022-06-11 20:27:37 +0200

    Add nullable

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -3,7 +3,7 @@ namespace Todo.Models
     public class TodoItem
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Deadline {get; set;}
+        public string? Name { get; set; }
+        public string? Deadline {get; set;}
     }
 }
--- Models/ViewModels/TodoViewModel.cs
+++ Models/ViewModels/TodoViewModel.cs
@@ -2,7 +2,7 @@ namespace Todo.Models.ViewModels
 {
     public class TodoViewModel
     {
-        public List<TodoItem> TodoList { get; set; }
-        public TodoItem Todo { get; set; }
+        public List<TodoItem>? TodoList { get; set; }
+        public TodoItem? Todo { get; set; }
     }
 }
